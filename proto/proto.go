5c1a1bac8db5e4214e41c83016201d3f9465ea2e
LonelySnail <695943455@qq.com>
2019-06-06 09:56:50 +0800

    Update proto.go
    slice string

--- proto/proto.go
+++ proto/proto.go
@@ -121,3 +121,16 @@ func UnPacket(r io.Reader)  error {
 	fmt.Println(string(msg.payLoad),"3333333333333")
 	return  nil
 }
+
+
+
+func SliceByteToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func StringToSliceByte(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
